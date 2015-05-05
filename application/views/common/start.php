cdc5c7f5c7fcc1ee5a0724864ab77be80ad32f37
ryan <ryan@ryankoon.com>
2015-05-04 18:46:35 -0700

    Set up main controller, common view files for start and end of each html view

---
+++ application/views/common/start.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<?php /* HEAD ------------------------------> */ ?>
+<head>
+	<!-- CSS -->
+   	<link rel="stylesheet" href="<?php echo base_url()?>styles/common.css">
+
+   	<!-- jQuery -->
+   	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+</head>
+
+
+
+<?php /* BODY ------------------------------> */ ?>
+<body>
