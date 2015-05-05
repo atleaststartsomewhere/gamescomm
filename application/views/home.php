4b5a51e3b27aabce1562864b143ff478171d4d66
Andrew Pyle <andy.pyle@gmail.com>
2015-05-04 19:06:56 -0700

    Initial commit for Front End.

---
+++ application/views/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta charset="UTF-8">
+	<title>GamesComm</title>
+	<!-- Vendor CSS -->
+	<!-- - Materialize -->
+	<!-- - FontAwesome -->
+
+	<!-- Gamescomm CSS -->
+
+</head>
+<body>
+	
+
+	<!-- Vendor JS -->
+	<!-- - jQuery -->
+	<!-- - Angular -->
+
+
+	<!-- Gamescomm JS -->
+</body>
+</html>
